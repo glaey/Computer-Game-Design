b28808f84ad5c240e8268215c255302dc0947efb
cinna-zju <569095288@qq.com>
2019-11-21 19:47:17 +0100

    Add dialogue

---
+++ Example/Assets/Scripts/dlgTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dlgTrigger : MonoBehaviour
+{
+  public mDialogue dialogue;
+
+  public void TriggerDialogue()
+  {
+    FindObjectOfType<dlgMgr>().StartDialogue(dialogue);
+  }
+
+}
--- Example/Assets/Scripts/interactible.cs
+++ Example/Assets/Scripts/interactible.cs
@@ -4,33 +4,52 @@ using UnityEngine;
 
 public class interactible : MonoBehaviour
 {
-    Vector3 position;
-    public GameObject spriteObj;
-    public float height = 1.0f;
-    // Start is called before the first frame update
-    void Start()
+  Vector3 position;
+  public GameObject spriteObj;
+  public float height = 1.0f;
+  // Start is called before the first frame update
+  void Start()
+  {
+    spriteObj.transform.rotation = GameObject.Find("Main Camera").transform.rotation;
+    spriteObj.transform.position = gameObject.transform.position + new Vector3(0f, height, 0f);
+    spriteObj.SetActive(false);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (spriteObj.activeSelf)
     {
-        spriteObj.transform.rotation = GameObject.Find("Main Camera").transform.rotation;
-        spriteObj.transform.position = gameObject.transform.position + new Vector3(0f, height, 0f);
-        spriteObj.SetActive(false);
+
+      if (Input.GetKeyDown("j"))
+      {
+        GameObject.Find("dialogManager").GetComponent<dlgMgr>().DisplayNextSentence();
+
+
+
+
+      }
+      if (Input.GetKeyDown("k"))
+      {
+
+      }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  }
 
-    void OnTriggerEnter(Collider other)
-    {
-        print("show");
-        spriteObj.SetActive(true);
-        
-    }
+  void OnTriggerEnter(Collider other)
+  {
+    print("show");
+    spriteObj.SetActive(true);
+    GetComponent<dlgTrigger>().TriggerDialogue();
 
-    void OnTriggerExit(Collider other)
-    {
-        print("hide");
-        spriteObj.SetActive(false);
-    }
+
+  }
+
+  void OnTriggerExit(Collider other)
+  {
+    print("hide");
+    spriteObj.SetActive(false);
+  }
 }
