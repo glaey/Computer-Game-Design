f613e7730695dbed76858c8cfa17676ac22d05bc
Eimutt <emilbojonsson@gmail.com>
2019-12-04 00:17:36 +0100

    Fixed wolf howling

--- Example/Assets/Scripts/Critter.cs
+++ Example/Assets/Scripts/Critter.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class Critter : MonoBehaviour
 {
+    ParticleSystem particleSystem;
+    Animator animator;
+    Vector3 _moveDir;
+    public float speed;
+    bool scared = false;
+    float scareSum = 0;
+    float scareDur;
+    public float scaredCoef = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        particleSystem = GetComponent<ParticleSystem>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (scared)
+        {
+            Move();
+            scareSum += Time.deltaTime;
+            if(scareSum > scareDur)
+            {
+                scared = false;
+                scareSum = 0;
+                animator.SetBool("isRunning", false);
+            }
+        }
     }
 
-    public void Move(Vector3 moveDir)
+    public void Move()
     {
-        transform.position += moveDir * 0.1f;
+        transform.position += _moveDir * speed * Time.deltaTime;
+    }
+
+    public void Scare(Vector3 moveDir, float duration)
+    {
+        transform.rotation = Quaternion.LookRotation(new Vector3(moveDir.x, 0, moveDir.z), new Vector3(0,1f,0));
+        particleSystem.Play();
+        _moveDir = moveDir;
+        scareDur = duration * scaredCoef;
+        scared = true;
+        animator.SetBool("isRunning", true);
     }
 }
