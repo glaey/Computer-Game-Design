ed475c3361096030dbfa03e490cbc530e96aacec
Eimutt <emilbojonsson@gmail.com>
2019-12-06 15:56:49 +0100

    Made only girl able to call

---
+++ Example/Assets/Girl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+    public GameObject[] penguins;
+    // Start is called before the first frame update
+    void Start()
+    {
+        penguins = GameObject.FindGameObjectsWithTag("Baby");
+        particleSystem = gameObject.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C) || Input.GetKeyDown(KeyCode.JoystickButton3))
+        {
+            particleSystem.Play(false);
+            particleSystem.loop = true;
+            Call();
+        }
+        else if (Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.JoystickButton3))
+        {
+            
+            Call();
+        }
+        else if (Input.GetKeyUp(KeyCode.C) || Input.GetKeyUp(KeyCode.JoystickButton3))
+        {
+            particleSystem.loop = false;
+           
+        }
+    }
+
+    void Call()
+    {
+        for (int i = 0; i < penguins.Length; i++)
+        {
+            penguins[i].GetComponent<Call>().Move(transform.position);
+        }
+    }
+}
--- Example/Assets/Scripts/Call.cs
+++ Example/Assets/Scripts/Call.cs
@@ -7,6 +7,7 @@ public class Call : MonoBehaviour
     public GameObject target;
     public float speed;
     Animator animator;
+    bool ismoving;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class Call : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.JoystickButton3))
         {
             gameObject.transform.position = gameObject.transform.position + ((target.gameObject.transform.position - gameObject.transform.position).normalized * speed * Time.deltaTime);
@@ -24,6 +26,22 @@ public class Call : MonoBehaviour
         } else
         {
             animator.SetBool("isRunning", false);
+        }*/
+        if (ismoving)
+        {
+            animator.SetBool("isRunning", true);
         }
+        else
+        {
+            animator.SetBool("isRunning", false);
+        }
+        ismoving = false;
+    }
+
+    public void Move(Vector3 target)
+    {
+        gameObject.transform.position = gameObject.transform.position + ((target - gameObject.transform.position).normalized * speed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation((target - gameObject.transform.position), new Vector3(0, 1f, 0));
+        ismoving = true;
     }
 }
