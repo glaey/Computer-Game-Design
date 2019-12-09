7692b47340d73d9572df2685be593e63dbb551b4
Eimutt <emilbojonsson@gmail.com>
2019-12-09 11:48:30 +0100

    Reset Keyboard

--- Example/Assets/Scripts/Reset.cs
+++ Example/Assets/Scripts/Reset.cs
@@ -25,7 +25,7 @@ public class Reset : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Joystick1Button6))
+        if (Input.GetKeyDown(KeyCode.Joystick1Button6) || Input.GetKeyDown(KeyCode.K))
         {
             if (isMainLevel)
             {
