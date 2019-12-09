7b8af5b9cc424b92c5e7a26ff462c89431703632
Eimutt <emilbojonsson@gmail.com>
2019-12-09 12:12:57 +0100

    Win state

--- Example/Assets/Scripts/inventory.cs
+++ Example/Assets/Scripts/inventory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class inventory : MonoBehaviour
@@ -9,6 +10,9 @@ public class inventory : MonoBehaviour
     public int yellowGoal;
     int purpleMush = 0;
     int yellowMush = 0;
+    private bool lvl1;
+    private bool lvl2;
+    private bool lvl3;
     // Start is called before the first frame update
     GameObject manager;
     void Start()
@@ -35,7 +39,7 @@ public class inventory : MonoBehaviour
         purpleText=gameObject.transform.Find("PurpleMushroom").Find("Amount").GetComponent<Text>();
         purpleText.text = purpleMush.ToString();
         if(purpleMush>=purpleGoal)purpleText.text="<color=#1E6C1D>"+purpleText.text+"</color>";
-        
+        CheckIfWin();
     }
     public void pickUpYellow()
     {
@@ -44,8 +48,8 @@ public class inventory : MonoBehaviour
 
         yellowText=gameObject.transform.Find("YellowMushroom").Find("Amount").GetComponent<Text>();
         yellowText.text = yellowMush.ToString();
-         if(yellowMush>=purpleGoal)yellowText.text="<color=#1E6C1D>"+yellowText.text+"</color>";
-
+        if(yellowMush>=purpleGoal)yellowText.text="<color=#1E6C1D>"+yellowText.text+"</color>";
+        CheckIfWin();
     }
     public void pickUpRed()
     {
@@ -53,7 +57,8 @@ public class inventory : MonoBehaviour
 
         redText=gameObject.transform.Find("RedCrystal").Find("Amount").GetComponent<Text>();
         redText.text="<color=#1E6C1D>"+1+"</color>";
-
+        lvl1 = true;
+        CheckIfWin();
     }
     public void pickUpGreen()
     {
@@ -61,7 +66,8 @@ public class inventory : MonoBehaviour
 
         greenText=gameObject.transform.Find("GreenCrystal").Find("Amount").GetComponent<Text>();
         greenText.text="<color=#1E6C1D>"+1+"</color>";
-
+        lvl2 = true;
+        CheckIfWin();
     }
 
     public void pickUpPenguin()
@@ -70,7 +76,18 @@ public class inventory : MonoBehaviour
 
         greenText = gameObject.transform.Find("Penguin").Find("Amount").GetComponent<Text>();
         greenText.text = "<color=#1E6C1D>" + 1 + "</color>";
+        lvl3 = true;
+        CheckIfWin();
 
     }
+
+    public void CheckIfWin()
+    {
+        if(lvl1 && lvl2 & lvl3 && (purpleMush >= purpleGoal ) && (yellowMush >= yellowGoal))
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
 
 }
