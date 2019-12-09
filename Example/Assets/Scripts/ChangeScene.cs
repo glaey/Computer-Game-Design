e1f8f24c154b51c06fd5a947348346bcc44afdbb
Eimutt <emilbojonsson@gmail.com>
2019-12-09 02:35:11 +0100

    Improved interactibles

--- Example/Assets/Scripts/ChangeScene.cs
+++ Example/Assets/Scripts/ChangeScene.cs
@@ -8,6 +8,9 @@ public class ChangeScene : MonoBehaviour
     public string SceneName;
     public bool tutorial;
     public bool toLevel;
+    private bool activated;
+    private float buffer = 1f;
+    private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,36 +19,44 @@ public class ChangeScene : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-
-        if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.F))
+        if (timer > buffer)
         {
-            if (tutorial)
+            if (other.transform.parent.tag == "Player" && (Input.GetKey(KeyCode.Joystick1Button2) || Input.GetKey(KeyCode.F)))
             {
-                SceneManager.LoadScene(SceneName);
-            }
-            else
-            {
-                if (toLevel)
-                {
-                    StartCoroutine(LoadLevel());
-                }
-                else
-                {
-                    StartCoroutine(LoadMainLevel());
-                }
+                activated = true;
             }
         }
     }
     // Update is called once per frame
     void OnTriggerStay(Collider other)
     {
-        
-        if (Input.GetKeyDown(KeyCode.Joystick1Button2) || Input.GetKeyDown(KeyCode.F))
+        if(timer > buffer)
         {
+            if (other.transform.parent.tag == "Player" && (Input.GetKey(KeyCode.Joystick1Button2) || Input.GetKey(KeyCode.F)))
+            {
+                activated = true;
+            }
+        }
+    }
+    
+    void OnEnable()
+    {
+        activated = false;
+        timer = 0;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (activated && timer > buffer)
+        {
+            activated = false;
+            timer = 0;
             if (tutorial)
             {
                 SceneManager.LoadScene(SceneName);
-            } else
+            }
+            else
             {
                 if (toLevel)
                 {
